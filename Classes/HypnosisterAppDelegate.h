10f8af13b5947656592a50707af5a46acb52c336
John Shields <rjs@agilitec-inc.com>
2011-01-25 22:23:50 -0600

    initial project creation

---
+++ Classes/HypnosisterAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by John Shields on 1/25/11.
+//  Copyright 2011 Agilitec Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
